7f8573ef7b206598881e3323233fa0c0ffacb4f3
Eden Mugg <brillianteden@hotmail.com>
2018-02-12 21:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function johnLennonFacts(array) {
 
 function iLoveTheBeatles(n) {
   var array = [];
-  while (i < n) {
-    do array.push("I love the Beatles!");
-    i++;
-    }
+  do {
+    array.push('I love the Beatles!')
+    n++
+  } while (n < 15)
   return array;
 }
